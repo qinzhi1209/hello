d25da4474b0a5f4e5f158c1973477a4951aa8c93
joyrun <joyrun@joyrundeMacBook-Pro-3.local>
2022-08-09 15:51:27 +0800

    feat:SayHi现在支持向指定人打招呼了

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/qinzhi1209/hello
+module github.com/qinzhi1209/hello/v2
 
 go 1.18
--- hello.go
+++ hello.go
@@ -2,6 +2,6 @@ package hello
 
 import "fmt"
 
-func SayHi()  {
-	fmt.Println("你好，我是秋天不回来！很高兴认识你")
+func SayHi(name string)  {
+	fmt.Println("你好%s，我是秋天不回来！很高兴认识你 \n",name)
 }
